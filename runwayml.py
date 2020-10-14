ebaa48e244ccf85a8366d6740b9743b16e7af92c
Larry Davis <lddavisjr@gmail.com>
2020-10-14 17:23:22 -0500

    Fix image encoding problem

--- runwayml.py
+++ runwayml.py
@@ -1,7 +1,8 @@
 from os import path, getenv
 from json import dumps, loads
-from base64 import b64encode, b64decode
+from base64 import b64encode, b64decode, decodebytes
 from time import time
+from re import sub, match
 from urllib.request import urlopen, Request
 from dotenv import load_dotenv
 from requests import get, post
@@ -19,43 +20,61 @@ else:
     raise RuntimeError("No .env file found and no environment variable called MODEL_TOKEN exists")
 
 
-def get_model_information(token=model_token):
-    get_request = get('https://munit-395cdb2f.hosted-models.runwayml.cloud/v1/info',
+def load_image(filename='huey.jpg'):
+    file_types = ['png', 'jpg', 'jpeg']
+    name, extension = filename.split('.')
+
+    if str.lower(extension) not in file_types:
+        raise RuntimeError('Only JPEG and PNG images can be used. Also, naming pattern must be <name>.<extension> '
+                           'with extension png, jpg, or jpeg')
+
+    with open(filename, 'rb') as fp:
+        input_data = b64encode(fp.read())
+
+    return input_data
+
+
+def get_model_information(url, token=model_token):
+    get_request = get(url,
                       headers={
                           "Accept": "application/json",
                           "Content-Type": "application/json",
                           "Authorization": "Bearer " + token
                       })
 
-    model_information = get_request.json()
-    print(model_information)
+    information = get_request.json()
+    return information
 
 
-def query_model_with_requests(image_data, token=model_token):
+def query_model_with_requests(url, image_data, token=model_token):
     print('Querying the model with requests...')
-    encoded_data = b64encode(image_data)
-    utf8encode = str(encoded_data).encode("utf-8")
-    print(type(utf8encode))
+    decoded_data = image_data.decode('utf-8')
 
-    model_input = {'image': utf8encode, 'style': 42}
+
+
+    model_input = {'image': decoded_data, 'style': 42}
 
     query_start = time()
     print('Starting model query...')
-    post_request = post('https://munit-395cdb2f.hosted-models.runwayml.cloud/v1/query',
+    post_request = post(url,
                         headers={
                             "Accept": "application/json",
                             "Content-Type": "application/json",
                             "Authorization": "Bearer " + token
                         },
-                        # data=json.dumps(model_input).encode('utf8'))
-                        data=model_input)
+                        data=dumps(model_input).encode('utf8'))
 
     query_duration = time() - query_start
     print('Done!! Elapsed time: {0}'.format(query_duration))
-    print(post_request.json())
+    # print(post_request.json())
     model_output = post_request.json()
 
-    result = b64decode(model_output['image'])
+    result_string = model_output['image']
+
+    # remove the first 23 characters containing the encoding information in the string so we can properly encode
+    result = sub('data:image/jpeg;base64,', '', result_string)
+    if len(result) == len(result_string):  # The string wasn't found and no replacement was made
+        raise RuntimeError('Unable to find image encoding from API')
 
     return result
 
@@ -91,12 +110,24 @@ def query_model_with_urllib(image_data, token=model_token):
 
 
 if __name__ == '__main__':
-    with open('huey.jpg', 'rb') as fp:
-        input_data = fp.read()
 
-    get_model_information()
-    image = query_model_with_requests(input_data)
-    # image = query_model_with_urllib(input_data)
+    # filename = 'lt.png'
+    # file_types = ['png', 'jpg', 'jpeg']
+    # name, extension = filename.split('.')
+    #
+    # if str.lower(extension) not in file_types:
+    #     raise RuntimeError('Only JPEG and PNG images can be used. Also, naming pattern must be <name>.<extension> '
+    #                        'with extension png, jpg, or jpeg')
+    #
+    # with open(filename, 'rb') as fp:
+    #     input_data = b64encode(fp.read())
+
+    info_url = 'https://munit-395cdb2f.hosted-models.runwayml.cloud/v1/info'
+    query_url = 'https://munit-395cdb2f.hosted-models.runwayml.cloud/v1/query'
+
+    input_data = load_image()
+    model_information = get_model_information(info_url)
+    result = query_model_with_requests(query_url, input_data)
 
     with open('output_image.jpg', 'wb') as output:
-        output.write(image)
+        output.write(decodebytes(result.encode('utf-8')))
